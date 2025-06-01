3cde64dc612e0dfdf0b4ee61564b440b9fbcd38d
Ashrafe <achraf.moradi@gmail.com>
2025-06-02 01:21:24 +0200

    Ajout des apps principales et configuration de INSTALLED_APPS

--- OcciPlace/settings.py
+++ OcciPlace/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Apps du projet OcciPlace
+    'accounts',
+    'products',
+    'orders',
+    'wishlist',
+    'notifications',
+    'loyalty',
+    #'blog',
 ]
 
 MIDDLEWARE = [
